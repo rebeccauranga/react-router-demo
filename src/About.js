222f5f1640f85827976c61ff56eda4e4e348243d
Rebecca Uranga <urangarebecca@gmail.com>
2019-04-30 15:23:53 -0400

    creates initial routes

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function About(props) {
+    return(
+        <div>
+            <h1>About Us</h1>
+        </div>
+    );
+}
+
+
+export default About;
---
+++ src/Cats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Cats(props) {
+    return(
+        <div>
+            <h1>Look at all those chickenths</h1>
+            <img src="https://bit.ly/2UMk9tS" />
+        </div>
+    );
+}
+
+
+export default Cats;
